7eb668e00fe5e382e7231a6ab80fe603d298811d
Pele <banskimateusz7@gmail.com>
2023-10-04 00:16:18 +0200

    Update AppRoutes.js, add components files and remove unnecessary components

--- JBZD-Clone/ClientApp/src/AppRoutes.js
+++ JBZD-Clone/ClientApp/src/AppRoutes.js
@@ -1,6 +1,10 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
+import { ToAcceptPostPage } from "./components/ToAcceptPostPage";
+import { RandomPostPage } from "./components/RandomPostPage";
+import { TermsOfService } from "./components/TermsOfService";
+import { Contact } from "./components/Contact";
+import { Changelog } from "./components/Changelog";
+import { FAQ } from "./components/FAQ";
 
 const AppRoutes = [
   {
@@ -8,12 +12,28 @@ const AppRoutes = [
     element: <Home />
   },
   {
-    path: '/counter',
-    element: <Counter />
+      path: "/to-accept",
+      element: <ToAcceptPostPage />
   },
   {
-    path: '/fetch-data',
-    element: <FetchData />
+      path: "/random",
+      element: <RandomPostPage />
+  },
+  {
+      path: "/terms-of-service",
+      element: <TermsOfService />
+  },
+  {
+      path: "/contact",
+      element: <Contact />
+  },
+  {
+      path: "/changelog",
+      element: <Changelog />
+  },
+  {
+      path: "/faq",
+      element: <FAQ />
   }
 ];
 
